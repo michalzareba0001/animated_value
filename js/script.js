ef610411e25d620ab9e15ed38e450777775f1ca7
Michal <michalzareba0001@gmail.com>
2024-03-23 13:06:53 +0100

    first animation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let rangeOne = document.querySelector('#rangeOne');
-    let valueContainer = document.querySelector('#valueContainer')
+    const rangeOne = document.querySelector('#rangeOne');
+    const valueContainer = document.querySelector('.value-container');
+    const valueTable = document.querySelector('#tableOne');
 
-    rangeInput.addEventListener('input', updateValue);
+    let previousValue = 0; // Przechowuje poprzednią wartość dla porównania
+    let currentTop = 0; // Przechowuje aktualną pozycję w pionie valueTable
 
     let updateValue = () => {
-        
+        const currentValue = rangeOne.value;
+        valueContainer.innerHTML = currentValue;
+
+        // Wyznacz kierunek przesunięcia
+        const direction = currentValue > previousValue ? 1 : -1; // 1 w górę, -1 w dół
+
+        // Aktualizuj pozycję w pionie
+        currentTop += direction * -50;  // Przesuń o 10px w wyznaczonym kierunku
+        valueTable.style.top = currentTop + 'px';
+
+        // Zapisz aktualną wartość dla następnego porównania
+        previousValue = currentValue;
     }
 
 
-})
+    rangeOne.addEventListener('input', updateValue);
+});

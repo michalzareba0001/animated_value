a4b6faa56db896fa24973c68180b6b912871f327
Michal <michalzareba0001@gmail.com>
2024-03-23 10:40:21 +0100

    let's start

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let rangeOne = document.querySelector('#rangeOne');
+    let valueContainer = document.querySelector('#valueContainer')
+
+    rangeInput.addEventListener('input', updateValue);
+
+    let updateValue = () => {
+        
+    }
+
+
+})
